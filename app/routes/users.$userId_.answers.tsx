08ac426ce1f6867725b105194767256cae8e4baf
quang <quanglng2022@gmail.com>
2023-12-27 14:34:36 +0700

    Add empty state

--- app/routes/users.$userId_.answers.tsx
+++ app/routes/users.$userId_.answers.tsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "@remix-run/react";
 import { loader } from "./resources.users.$userId.answers";
 import Answer from "~/components/Answer";
+import { Typography } from "@mui/material";
 
 export { loader };
 
@@ -8,6 +9,11 @@ const Answers = () => {
   const data = useLoaderData<typeof loader>();
   return (
     <div className="flex flex-col w-full h-full">
+      {data.submission.data.items.length === 0 && (
+        <Typography sx={{ fontSize: 18 }}>
+          Có vẻ bạn chưa có câu trả lời nào
+        </Typography>
+      )}
       {data.submission.data.items.map((answer) => (
         <Answer key={answer.id} answer={answer} />
       ))}
--- app/routes/users.$userId_.questions.tsx
+++ app/routes/users.$userId_.questions.tsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "@remix-run/react";
 import { loader } from "./resources.users.$userId.questions";
 import { Question } from "~/components";
+import { Typography } from "@mui/material";
 
 export { loader };
 
@@ -8,6 +9,11 @@ const Questions = () => {
   const data = useLoaderData<typeof loader>();
   return (
     <div className="flex flex-col w-full h-full">
+      {data.submission.data.items.length === 0 && (
+        <Typography sx={{ fontSize: 18 }}>
+          Có vẻ bạn chưa có câu hỏi nào
+        </Typography>
+      )}
       {data.submission.data.items.map((question) => (
         <Question key={question.id} question={question} />
       ))}
